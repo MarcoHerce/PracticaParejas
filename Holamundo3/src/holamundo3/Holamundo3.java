8636bfd714d4f22d57fd7fdc7f2fff268ed31e55
Diurno <Diurno@C8PC9.informatica.comercio>
2021-02-10 10:36:38 +0100

    version 2, añadimos clase clase2,

---
+++ Holamundo3/src/holamundo3/Holamundo3.java
@@ -0,0 +1,17 @@
+
+package holamundo3;
+
+/**
+ * 
+ * @authors Artem y Marco
+ */
+public class Holamundo3 {
+
+    /**
+     *  
+     */
+    public static void main(String[] args) {
+       
+    }
+    
+}
